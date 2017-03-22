773578c181f0b2a2582310f4047335a9d0249105
Anatoliy Klots and Max Reichman <anatoliy@>
2017-03-22 14:36:22 -0700

    create survey with four parameteres

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   $("#survey").submit(function(event) {
     var nameInput = $("input#name").val();
+    var favoriteFood = $("#food").val();
+    var bestTeam = $("input:radio[name=team]:checked").val();
+    var birthYear = $("#born").val();
+    var bestColor = $("#color").val();
 
     $(".name").text(nameInput);
-
-    $("#letterOutput").show();
+    $(".food").text(favoriteFood);
+    $(".team").text(bestTeam);
+    $(".born").text(birthYear);
+    $(".color").text(bestColor);
 
     event.preventDefault();
   });
